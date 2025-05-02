0590f548998b58444a4399fade7daa62209ee358
YesayaSitompul <2381050@unai.edu>
2025-05-02 09:10:05 +0700

    backend

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: 'https://final-book-review-hub-fe-98vb.vercel.app/', // ganti dengan domain frontend kamu
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
